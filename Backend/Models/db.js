66493d75e633f63468bc2dba42bb837e0e5613aa
Chirag Kuinkel <Chiragkuinkel3@gmail.com>
2024-10-01 09:19:03 +0545

    2nd commit

--- Backend/Models/db.js
+++ Backend/Models/db.js
@@ -1,1 +1,11 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
+const mongo_url = process.env.MONGO_CONN;
+mongoose.connect(mongo_url).then(
+    ()=>{
+        console.log("Mongodb connected...");
+
+    }).catch((err)=>{
+        console.log("MongoDB connection error:",err);
+    }
+)
